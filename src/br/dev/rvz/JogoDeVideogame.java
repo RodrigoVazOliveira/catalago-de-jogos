8f54a9562885a4d3ce891f50e7cc18f1afa164b8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-09 15:29:45 -0300

    implementado modelo de jgoo de video game

--- src/br/dev/rvz/JogoDeVideogame.java
+++ src/br/dev/rvz/JogoDeVideogame.java
@@ -1,4 +1,25 @@
 package br.dev.rvz;
 
-public class JogoDeVideogame {
+/**
+ * classe responsavel pelo jogo de video games
+ * @author Rodrigo.vaz
+ * */
+public class JogoDeVideogame extends Jogo {
+    private boolean online;
+
+    public JogoDeVideogame() {
+    }
+
+    public JogoDeVideogame(String nome, int quantidadeDeJogo, int horaDeJogo, TipoDeJogo tipoDeJogo, boolean online) {
+        super(nome, quantidadeDeJogo, horaDeJogo, tipoDeJogo);
+        this.online = online;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
 }
