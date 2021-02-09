fda231b06f44b9be777ba27c54f9653d06fa6313
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-09 15:33:52 -0300

    implementaod modelo de jogo de rua e enum para local de brincar

---
+++ src/br/dev/rvz/JogoDeRua.java
@@ -0,0 +1,34 @@
+package br.dev.rvz;
+
+/**
+ * modelo de jgoo de rua
+ * */
+public class JogoDeRua extends Jogo{
+    private boolean perigosa;
+    private LocalparaBrinca localparaBrinca;
+
+    public JogoDeRua() {
+    }
+
+    public JogoDeRua(String nome, int quantidadeDeJogo, int horaDeJogo, TipoDeJogo tipoDeJogo, boolean perigosa, LocalparaBrinca localparaBrinca) {
+        super(nome, quantidadeDeJogo, horaDeJogo, tipoDeJogo);
+        this.perigosa = perigosa;
+        this.localparaBrinca = localparaBrinca;
+    }
+
+    public boolean isPerigosa() {
+        return perigosa;
+    }
+
+    public void setPerigosa(boolean perigosa) {
+        this.perigosa = perigosa;
+    }
+
+    public LocalparaBrinca getLocalparaBrinca() {
+        return localparaBrinca;
+    }
+
+    public void setLocalparaBrinca(LocalparaBrinca localparaBrinca) {
+        this.localparaBrinca = localparaBrinca;
+    }
+}
---
+++ src/br/dev/rvz/LocalparaBrinca.java
@@ -0,0 +1,9 @@
+package br.dev.rvz;
+
+/**
+ * padrão d elugares para brincar
+ * @author Rodrigo.vaz
+ * */
+public enum LocalparaBrinca {
+    RUA, PASSEIO, QUADRA, RAMPA_SKATE,  CIRCUITO_BICICLETA;
+}
