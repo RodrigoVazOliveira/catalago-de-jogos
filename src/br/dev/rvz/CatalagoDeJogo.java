e3b798ce52c8beb081f01f4677a8c8a0694843e1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-09 15:36:33 -0300

    implmentado a entidade responsavel ao gerenciamento de catalago de jogos

---
+++ src/br/dev/rvz/CatalagoDeJogo.java
@@ -0,0 +1,24 @@
+package br.dev.rvz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  gerenciamento de catalagos dos jogos cadastrsos
+ * @author Rodrigo.vaz
+ * */
+public class CatalagoDeJogo {
+    private List<Jogo> catalagoJogos;
+
+    CatalagoDeJogo() {
+        catalagoJogos = new ArrayList<>();
+    }
+
+    public List<Jogo> getCatalagoJogos() {
+        return catalagoJogos;
+    }
+
+    public void setCatalagoJogos(List<Jogo> catalagoJogos) {
+        this.catalagoJogos = catalagoJogos;
+    }
+}
